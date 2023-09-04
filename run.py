1772858588b11be38bf7c9035bc40bb19c87c1e2
Woergaard <thomas.w.kjaer@gmail.com>
2023-09-04 18:16:13 +0200

    Ex1 - 1. commit

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 import os
-
+"""
 with open('commit_number.txt', 'r+') as f:
     commit_number = int(f.read())
     f.seek(0)
     f.write(str(commit_number + 1))
     f.truncate()
+"""
 
+commit_number = 1
 os.system('git add .')
 commit_message = 'Ex1 - ' + str(commit_number) + '. commit'
 os.system('git commit -a -m "' + commit_message + '"')
