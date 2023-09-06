f3a7659c04aa45c6e40f27e186b8849045b1bf4a
Woergaard <thomas.w.kjaer@gmail.com>
2023-09-06 14:59:59 +0200

    Ex1 - 106. commit

--- Ex2/Ex2.py
+++ Ex2/Ex2.py
@@ -17,17 +17,17 @@ def sensorReadings():
 
 
         # request to read Back sonar ping sensor
-        print("Back sensor = ", arlo.read_back_ping_sensor())
-        sleep(0.041)
+#        print("Back sensor = ", arlo.read_back_ping_sensor())
+#        sleep(0.041)
 
         # request to read Right sonar ping sensorutils
 
-        print("Right sensor = ", arlo.read_right_ping_sensor())
-        sleep(0.041)
+ #       print("Right sensor = ", arlo.read_right_ping_sensor())
+ #       sleep(0.041)
 
         # request to read Left sonar ping sensor
-        print("Left sensor = ", arlo.read_left_ping_sensor())
-        sleep(0.041)
+ #       print("Left sensor = ", arlo.read_left_ping_sensor())
+ #       sleep(0.041)
 
         
 def drive_forward(start, oneMeterSeconds, meters):
