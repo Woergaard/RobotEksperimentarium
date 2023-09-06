ab93a0c3fd55b019e02800030140033c5bd96559
Woergaard <thomas.w.kjaer@gmail.com>
2023-09-06 15:23:00 +0200

    Ex1 - 107. commit

--- Ex2/Ex2.py
+++ Ex2/Ex2.py
@@ -15,21 +15,87 @@ def sensorReadings():
         print("Front sensor = ", arlo.read_front_ping_sensor())
         sleep(0.041)
 
-
         # request to read Back sonar ping sensor
-#        print("Back sensor = ", arlo.read_back_ping_sensor())
-#        sleep(0.041)
+        print("Back sensor = ", arlo.read_back_ping_sensor())
+        sleep(0.041)
 
         # request to read Right sonar ping sensorutils
 
- #       print("Right sensor = ", arlo.read_right_ping_sensor())
- #       sleep(0.041)
+        print("Right sensor = ", arlo.read_right_ping_sensor())
+        sleep(0.041)
 
         # request to read Left sonar ping sensor
- #       print("Left sensor = ", arlo.read_left_ping_sensor())
- #       sleep(0.041)
+        print("Left sensor = ", arlo.read_left_ping_sensor())
+        sleep(0.041)
 
-        
+
+
+# CAROLINE
+def sensor():
+    foran = arlo.read_front_ping_sensor()
+    bagved = arlo.read_back_ping_sensor()
+    højre = arlo.read_right_ping_sensor()
+    venstre = arlo.read_left_ping_sensor()
+    
+    return foran, bagved, højre, venstre
+
+def kør(meters):
+    leftWheelFactor = 0.3
+    rightWheelFactor = 0.3
+    start = time.perf_counter()
+     
+    arlo.go_diff(leftWheelFactor*50, rightWheelFactor*50, 1, 1)
+    isDriving = True
+    while (isDriving): # or some other form of loop
+        if (time.perf_counter() - start > oneMeterSeconds * meters): #drive x meters
+            isDriving = False
+#SLUT CAROLINE 
+
+
+
+# SOFIE 
+def sensorValues():
+        forwards = arlo.read_front_ping_sensor()
+        backwards = arlo.read_back_ping_sensor()
+        left = arlo.read_left_ping_sensor()
+        right = arlo.read_right_ping_sensor()
+
+        return forwards, backwards, left, right
+
+def go_and_sensor(seconds):
+    '''
+    Funktionen venter i den tid, som robotten skal udføre en bestemt action.
+    Samme tanke som Sleep, men kan måske sørge for den kan holde sig opdateret på sine omgivelser.
+    Argumenter:
+        start:    starttidspunkts
+        seconds:  sekunder, der skal ventes
+    '''
+    start = time.perf_counter()
+    isDriving = True
+    while (isDriving):
+        if (time.perf_counter() - start > seconds):
+            isDriving = False
+        forwards, backwards, left, right = sensorValues()
+        sensorLst = [forwards, backwards, left, right]
+        sleep(0.041)
+        for i in sensorLst < 200:
+            isDriving = False
+            return i, sensorLst, 
+
+def drive_forward_and_sense():
+    sensorNames = ['forwards', 'backwards', 'left', 'right']
+
+
+
+# SLUT SOFIE
+
+# ASGER
+
+def s
+
+# SLUT ASGER
+
+
 def drive_forward(start, oneMeterSeconds, meters):
     #arlo.go_diff(_utils.leftWheelFactor*50, _utils.rightWheelFactor*50, 1, 1)
     isDriving = True
--- README.md
+++ README.md
@@ -3,3 +3,22 @@ Repo til faget Robot Eksperimentarium på Københavns Universitet (2023).
 
 
 Tanker til at løse vores opgaver: 
+
+Exercise 2: 
+Opgave 1: 
+- sætte en grænse værdi til væg (200 millimeter)
+- Tjek de mulige veje den kan tage (højre, venstre, bagud) 
+- Sensor værdi er i millimeter. 
+
+
+| Foran | Højre | Venstre | Bag | Kommando |
+|-------|-------|---------|-----|----------|
+|   :white_check_mark:    |  :white_check_mark:     |    :white_check_mark:     |   :white_check_mark:  |    Kør frem      |
+|   :x:    |  :white_check_mark:     |    :white_check_mark:     |   :white_check_mark:  |   Kør til højre eller venstre     |
+|   :x:    |    :x:   |    :white_check_mark:     |   :white_check_mark:  |    Vend om og kør bagud       |
+|   :x:    |    :x:   |     :x:    |  :white_check_mark:   |    Vend om og kør bagud      |
+|   :x:    |    :x:   |      :x:   |    :x: |    STOP      |
+|     :white_check_mark:  |    :white_check_mark:   |    :white_check_mark:     |  :x:   |   Kør frem og til højre eller venstre     |
+|   :white_check_mark:    |    :white_check_mark:   |      :x:   |  :x:   |    Kør fremad/lidt til højre     |
+|   :white_check_mark:    |    :x:   |     :x:    |   :x:  |   Kør fremad    |
+
