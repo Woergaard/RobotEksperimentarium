04fd6f566806e8845ba0bdba01560658a1d7d5a8
Woergaard <thomas.w.kjaer@gmail.com>
2023-09-06 14:57:03 +0200

    Ex1 - 104. commit

---
+++ Ex2/_utils.py
@@ -0,0 +1,120 @@
+from time import sleep
+import robot
+import time
+
+### DEFINEREDE PARAMETRE ###
+arlo = robot.Robot()
+
+rightWheelFactor = 1.0
+leftWheelFactor = 1.06225
+standardSpeed = 50.0
+
+### PARAMETERUDREGNING ###
+
+def degreeToSeconds(degrees):
+    '''
+    Funktionen omregner grader, der skal drejes, til sekunder, der skal ventes.
+    Argumenter:
+        degrees:    grader, der skal drejes
+    '''
+    ninetyDegreeTurnSeconds = 0.95
+    return (ninetyDegreeTurnSeconds/90) * degrees
+
+def metersToSeconds(meters):
+    '''
+    Funktionen omregner meter, der skal køres, til sekunder, der skal ventes.
+    Argumenter:
+        meters:    meter, der skal køres
+    '''
+    oneMeterSeconds = 2.8
+    return oneMeterSeconds * meters
+
+### BEVÆGELSE ###
+
+def go(seconds):
+    '''
+    Funktionen venter i den tid, som robotten skal udføre en bestemt action.
+    Samme tanke som Sleep, men kan måske sørge for den kan holde sig opdateret på sine omgivelser.
+    Argumenter:
+        start:    starttidspunkts
+        seconds:  sekunder, der skal ventes
+    '''
+    start = time.perf_counter()
+    isDriving = True
+    while (isDriving):
+        if (time.perf_counter() - start > seconds):
+            isDriving = False
+
+def drive(direction, meters):
+    '''
+    Funktionen kører robotten et antal meter.
+    Argumenter:
+        start:    starttidspunkt
+        direction:  enten 'forwards' eller 'backwards'
+        meters:   meter, der skal køres
+    '''
+    if direction == 'forwards':
+        arlo.go_diff(leftWheelFactor*standardSpeed, rightWheelFactor*standardSpeed, 1, 1)
+    elif direction == 'backwards':
+        arlo.go_diff(leftWheelFactor*standardSpeed, rightWheelFactor*standardSpeed, 0, 0)
+
+    driveSeconds = metersToSeconds(meters)
+    go(driveSeconds)
+
+def sharp_turn(direction, degrees):
+    '''
+    Funktionen kører robotten et antal meter.
+    Argumenter:
+        start:    starttidspunkt
+        direction:  enten 'right' eller 'left'
+        degrees:   grader, der skal køres
+    '''
+    if direction == "right":
+        arlo.go_diff(leftWheelFactor*standardSpeed, rightWheelFactor*standardSpeed, 1, 0)
+    elif direction == "left":
+        arlo.go_diff(leftWheelFactor*standardSpeed, rightWheelFactor*standardSpeed, 0, 1)
+    
+    turnSeconds = degreeToSeconds(degrees)
+    go(turnSeconds)
+
+def soft_turn(direction, degrees):
+    '''
+    Funktionen kører robotten et antal meter.
+    Argumenter:
+        start:    starttidspunkt
+        direction:  enten 'right' eller 'left'
+        degrees:   grader, der skal køres
+    '''
+    if direction == "right":
+        arlo.go_diff(leftWheelFactor*100, rightWheelFactor*40, 1, 1)
+    elif direction == "left":
+        arlo.go_diff(leftWheelFactor*40, rightWheelFactor*100, 1, 1)
+
+    turnSeconds = degreeToSeconds(degrees)
+    go(turnSeconds)
+            
+### SAMMENSAT BEVÆGELSE ###
+
+def move_in_square(meters):
+    for _ in range(4):
+        drive(1.0)
+
+        sharp_turn('right', 90)
+    
+
+def move_around_own_axis(i): 
+    for _ in range(i):
+        sharp_turn('right', 360)
+                
+        sharp_turn('left', 360)
+
+
+def move_in_figure_eight(i):
+    soft_turn('right', 360)
+
+    soft_turn('left', 360)
+
+    for _ in range(i):   
+            soft_turn('right', 360)
+            
+            soft_turn('left', 360)
