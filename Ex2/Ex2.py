09d0335f36279acae1b22cdb765e18a6d2f1824e
Woergaard <thomas.w.kjaer@gmail.com>
2023-09-11 16:04:22 +0200

    Ex2 - 20. commit

--- Ex2/Ex2.py
+++ Ex2/Ex2.py
@@ -22,6 +22,37 @@ def sensor():
     left = arlo.read_left_ping_sensor()
     
     return front, back, right, left
+
+def drive( ):
+    arlo.go_diff(_utils.leftWheelFactor*50, _utils.rightWheelFactor*50, 1, 1)
+    while (pingFront > 300 and pingLeft > 200 and pingRight > 200):
+        pingFront, pingLeft, pingRight, pingBack = sensor()
+    # nu er der sket noget!!!
+    ## indsæt håndtering
+    if(pingFront <= 300):
+        randomDirection = np.random('right', 'left')
+        _utils.sharp_turn(randomDirection, 90.0)
+    elif (pingFront < 300 and pingLeft < 200):
+        _utils.sharp_turn('right', 90.0)
+    elif (pingFront < 300 and pingRight < 200):
+        _utils.sharp_turn('left', 90.0)
+    elif (pingLeft < 200):
+        _utils.sharp_turn('right', 45)
+    elif (pingRight < 200):
+        _utils.sharp_turn('left', 45)
+    elif (pingLeft < 200 and pingRight < 200):
+        _utils.sharp_turn('left', 180)
+    elif (pingFront < 300 and pingLeft < 200 and pingRight < 200):
+        _utils.sharp_turn('left', 180)        
+    drive()
+
+
+
+
+# right og left sensor afstand, kan go
+
+
+#### GAMLE ####
 #Wheel = 47.2 
 
 def left_turn_return_route(pingFront, pingLeft, pingRight, pingBack):
@@ -43,7 +74,7 @@ def turn_right_back_to_route(pingFrong, pingLeft, pingRight, pingBack, timeDrive
     sleep(timeDriven)
     _utils.sharp_turn('right', 90.0)
 
-"""
+
 def drive(): #wheel):
     arlo.go_diff(_utils.leftWheelFactor*50, _utils.rightWheelFactor*50, 1, 1)
     isDriving = True
@@ -61,51 +92,6 @@ def drive(): #wheel):
             
     
 drive()        
-"""
-
-
-def drive():
-    arlo.go_diff(_utils.leftWheelFactor*50, _utils.rightWheelFactor*50, 1, 1)
-    isDriving = True
-    while (isDriving): # or some other form of loop
-        pingFront = arlo.read_front_ping_sensor() 
-        pingLeft = arlo.read_left_ping_sensor()
-        pingRight = arlo.read_right_ping_sensor()
-        pingBack = arlo.read_back_ping_sensor()
-        
-        if (pingFront <= 200): 
-            _utils.sharp_turn('right', _utils.degreeToSeconds(90.0))
-            turnSeconds = _utils.degreeToSeconds(90.0) 
-            _utils.go(turnSeconds)
-            arlo.go_diff(_utils.leftWheelFactor*50, _utils.rightWheelFactor*50, 1, 1)
-            _utils.metersToSeconds(2.8)
-            arlo.stop()
-            _utils.sharp_turn('left', _utils.degreeToSeconds(90.0))
-            turnSeconds = _utils.degreeToSeconds(90.0) 
-            _utils.go(turnSeconds)
-            arlo.go_diff(_utils.leftWheelFactor*50, _utils.rightWheelFactor*50, 1, 1)
-
-            if (pingRight and pingFront<= 200):
-                _utils.sharp_turn('left', _utils.degreeToSeconds(90.0))
-                turnSeconds = _utils.degreeToSeconds(90.0) 
-                _utils.go(turnSeconds)
-                arlo.go_diff(_utils.leftWheelFactor*50, _utils.rightWheelFactor*50, 1, 1)
-                arlo.stop() 
-                # Turn left 
-                
-
-                if(pingLeft and pingFront and pingRight <= 200): 
-                    _utils.move_around_own_axis('left', 180) 
-                    arlo.stop()
-                    # Turn back
-                        
-                    if(pingBack <= 200):
-                        print("I am stuck")
-                        arlo.stop()
-                        # STOP STOP STOP 
-                
-
-drive()
                 
             
 # 1. Kør frem indtil pingFront <= 200
