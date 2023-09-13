265a83c75667abbebdea7f36b872ba0551ea5866
Woergaard <thomas.w.kjaer@gmail.com>
2023-09-13 11:30:40 +0200

    Ex2 - 33. commit

--- Ex2/Ex2.py
+++ Ex2/Ex2.py
@@ -28,23 +28,22 @@ def drive():
     pingFront, pingLeft, pingRight, pingBack = sensor()
     arlo.go_diff(_utils.leftWheelFactor*50, _utils.rightWheelFactor*50, 1, 1)
     dirLst = ['right', 'left']
-    while (pingFront > 450 and pingLeft > 350 and pingRight > 350):
+    while (pingFront > 350 and pingLeft > 250 and pingRight > 250):
         pingFront, pingLeft, pingRight, pingBack = sensor()
-        print(pingLeft)
-    if (pingFront < 450 and pingLeft < 350 and pingRight < 350):
+    if (pingFront < 350 and pingLeft < 250 and pingRight < 250):
         _utils.sharp_turn('left', 180.0)    
-    elif (pingFront < 450 and pingLeft < 350):
+    elif (pingFront < 350 and pingLeft < 250):
         _utils.sharp_turn('right', 90.0)
-    elif (pingFront < 450 and pingRight < 350):
+    elif (pingFront < 350 and pingRight < 250):
         _utils.sharp_turn('left', 90.0)
-    elif (pingLeft < 350 and pingRight < 350):
+    elif (pingLeft < 250 and pingRight < 250):
         _utils.sharp_turn('left', 180.0)
-    elif(pingFront <= 450):
+    elif(pingFront <= 350):
         randomDirection = random.choice(dirLst)
         _utils.sharp_turn(randomDirection, 90.0)
-    elif (pingLeft < 350): 
+    elif (pingLeft < 250): 
         _utils.sharp_turn('right', 45.0)
-    elif (pingRight < 350):
+    elif (pingRight < 250):
         _utils.sharp_turn('left', 45.0)
     
         
@@ -59,3 +58,23 @@ drive()
 ########################
 #      Exercise 2      #
 ########################
+
+'''
+ while (pingFront > 350 and pingLeft > 250 and pingRight > 250):
+        pingFront, pingLeft, pingRight, pingBack = sensor()
+    if (pingFront < 350 and pingLeft < 250 and pingRight < 250):
+        _utils.sharp_turn('left', 180.0)    
+    elif (pingFront < 350 and pingLeft < 250):
+        _utils.sharp_turn('right', 90.0)
+    elif (pingFront < 350 and pingRight < 250):
+        _utils.sharp_turn('left', 90.0)
+    elif (pingLeft < 250 and pingRight < 250):
+        _utils.sharp_turn('left', 180.0)
+    elif(pingFront <= 350):
+        randomDirection = random.choice(dirLst)
+        _utils.sharp_turn(randomDirection, 90.0)
+    elif (pingLeft < 250): 
+        _utils.sharp_turn('right', 45.0)
+    elif (pingRight < 250):
+        _utils.sharp_turn('left', 45.0)
+'''
