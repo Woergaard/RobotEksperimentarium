d45cbccf4bc156422c3c1be98f572e1a48f1d08e
Woergaard <thomas.w.kjaer@gmail.com>
2023-09-13 14:11:41 +0200

    Ex2 - 61. commit

--- Ex2/Ex2.py
+++ Ex2/Ex2.py
@@ -60,9 +60,9 @@ def sensor_measure():
         pingFront, pingBack, pingRight, pingLeft = sensor_ex()
         print('front', pingFront, 'left', pingLeft, 'right', pingRight, 'back', pingBack)
 
-#sensor_measure()
+sensor_measure()
 
-_utils.drive_and_sense()
+#_utils.drive_and_sense()
 
 ########################
 #      Exercise 2      #
