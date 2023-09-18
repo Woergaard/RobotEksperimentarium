0577ac519f0a05a3c424a21b1d2977d78a432c13
Woergaard <thomas.w.kjaer@gmail.com>
2023-09-18 14:12:37 +0200

    Ex3 - 6. commit

--- Ex3/Ex3.py
+++ Ex3/Ex3.py
@@ -1,7 +1,9 @@
-
+import pkg_resources
+pkg_resources.require("opencv-python==4.6.0.66")
 import cv2 # Import the OpenCV library
 import time
 from pprint import *
+import numpy as np
 
 try:
     import picamera2
@@ -11,6 +13,30 @@ except ImportError:
     exit(-1)
 
 print("OpenCV version = " + cv2.__version__)
+
+def height_of_QR_in_image(img):
+    '''
+    Funktionen returnerer højden af en box på et billede i pixels.
+    Argumenter:
+        image:  det givne billede
+    '''
+    arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+    
+    aruco_corners, _, _ = cv2.aruco.detectMarkers(img, arucoDict) 
+    print(aruco_corners)
+
+
+    # If at least one marker is detected
+    if len(aruco_corners) > 0:
+        for corner in aruco_corners:
+            # The corners are ordered as top-left, top-right, bottom-right, bottom-left
+            top_left = corner[0][0]
+            bottom_left = corner[0][3]
+            
+            # Compute the height in pixels
+            height = bottom_left[1] - top_left[1]
+            print("Height of the ArUco marker:", height, "pixels")
+
 
 def camera_setup():
     # Open a camera device for capturing
@@ -41,13 +67,74 @@ def camera_setup():
         
         # Show frames
         cv2.imshow(WIN_RF, image)
+
+        height_of_QR_in_image(image)
     
 camera_setup()
 
 
 
 
-### EX 1 ###
+### EX 3.1 ###
 
-def height_of_box():
-    #måler 
+# x = height of QR code in image
+# X = height of QR code in image irl
+# Z = dist
+# f = focal length
+"""
+def height_of_QR_in_image(img):
+    '''
+    Funktionen returnerer højden af en box på et billede i pixels.
+    Argumenter:
+        image:  det givne billede
+    '''
+    arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+    
+    aruco_corners, _, _ = cv2.aruco.detectMarkers(img, arucoDict) 
+    print(aruco_corners)
+
+
+    # If at least one marker is detected
+    if len(aruco_corners) > 0:
+        for corner in aruco_corners:
+            # The corners are ordered as top-left, top-right, bottom-right, bottom-left
+            top_left = corner[0][0]
+            bottom_left = corner[0][3]
+            
+            # Compute the height in pixels
+            height = bottom_left[1] - top_left[1]
+            print("Height of the ArUco marker:", height, "pixels")
+        
+    #arucoMarkerLength = 145  # andet ord for X
+    #intrincsic_matrix = 
+    #rvecs, tvecs, objPoints = cv2.aruco.estimatePoseSingleMarkers(aruco_corners, arucoMarkerLength, intrinsic_matrix, distortion_coeffs)
+    
+    # x = 
+    # return x # højde på billedet
+"""
+
+
+def focal_length(xlst, X, Zlst):
+    '''
+    Funktionen returnerer højden af en box på et billede i pixels.
+    Argumenter:
+        x = height of QR code in image
+        X = height of QR code in image irl
+        Z = dist
+    '''
+    flst = []
+    for i in range(len(xlst)):
+        flst.append(xlst[i] * (X/Zlst[i])) 
+
+    fmean = np.mean(flst)
+    fstd = np.std(flst)
+    return fmean, fstd 
+
+
+
+Zlst = [100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0, 1000.0] #length of distances
+X = 145.0 # height of QR code IRL
+
+xlst = [] # height of QR code in image
+
+
