a88a3a516995668a3f029c24cf475fe790ff7b52
Woergaard <thomas.w.kjaer@gmail.com>
2023-09-18 15:37:18 +0200

    Ex3 - 11. commit

--- Ex3/caro.py
+++ Ex3/caro.py
@@ -14,7 +14,9 @@ except ImportError:
 
 print("OpenCV version = " + cv2.__version__)
 
-
+'''
+Definere højden af QR koden, og beregner denne ved at kigge på tup-left og bottom-left corner. 
+'''
 def height_of_QR_in_image(img):
     '''
     Funktionen returnerer højden af en box på et billede i pixels.n
@@ -37,10 +39,13 @@ def height_of_QR_in_image(img):
             # Compute the height in pixels
             height = bottom_left[1] - top_left[1]
             print("Height of the ArUco marker:", height, "pixels")
-            print(top_left)
+
 
 
 def camera_setup():
+    '''
+    Funktionen definere kamerarets setup
+    '''
     # Open a camera device for capturing
     imageSize = (1280, 720)
     FPS = 30
@@ -74,76 +79,25 @@ def camera_setup():
     
 camera_setup()
 
-
-
-
-### EX 3.1 ###
-
-# See focal.py file in Ex3 folder
-
-### EX 3.2 ###
-
-### DEFINEREDE PARAMETRE ###
 import _utils
-import robot
-
-arlo = robot.Robot()
-
-rightWheelFactor = 1.0
-leftWheelFactor = 1.06225
-standardSpeed = 50.0
-
-def camera(command):
-    # Open a camera device for capturing
-    imageSize = (1280, 720)
-    FPS = 30
-    cam = picamera2.Picamera2()
-    frame_duration_limit = int(1/FPS * 1000000) # Microseconds
-    # Change configuration to set resolution, framerate
-    picam2_config = cam.create_video_configuration({"size": imageSize, "format": 'RGB888'},
-                                                                controls={"FrameDurationLimits": (frame_duration_limit, frame_duration_limit)},
-                                                                queue=False)
-    cam.configure(picam2_config) # Not really necessary
-    cam.start(show_preview=False)
-
-    print(cam.camera_configuration()) # Print the camera configuration in use
-
-    time.sleep(1)  # wait for camera to setup
-
-
-    # Open a window
-    WIN_RF = "Example 1"
-    cv2.namedWindow(WIN_RF)
-    cv2.moveWindow(WIN_RF, 100, 100)
-
-    while cv2.waitKey(4) == -1: # Wait for a key pressed event
-        image = cam.capture_array("main")
-        
-        # Show frames
-        cv2.imshow(WIN_RF, image)
-
-        if command == 'turn and watch':
-            turn_and_watch('left', image)
-
-
-def turn_and_watch(direction, img):
+def check_suroundings(img, directions):
+    ''' 
+    Funktionen tjekker om der er en Landmark i nærheden 
+    aruvo_corners - Hjørner på 
+    directions - 
     '''
-    Funktionen drejer om egen akse, indtil den har fundet et landmark OG der er frit.
-    Argumenter:
-        direction:  enten 'right' eller 'left'
-    '''
-    if direction == "right":
+    if directions == 'right': 
         r, l = 1, 0
-    elif direction == "left":
-        r, l = 0, 1
-    else:
-        print("Indtast enten 'left' eller 'right' som retning")
-        return
-
-    arlo.go_diff(leftWheelFactor*standardSpeed, rightWheelFactor*standardSpeed, r, l)
     
+    if directions == 'left': 
+        l, r = 0, 1 
+        
     arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
-    aruco_corners, _, _ = cv2.aruco.detectMarkers(img, arucoDict)
+    
+    aruco_corners, _, _ = cv2.aruco.detectMarkers(img, arucoDict) 
+    print(aruco_corners)
+
+
     # If at least one marker is detected
     if len(aruco_corners) > 0:
         for corner in aruco_corners:
@@ -152,19 +106,14 @@ def turn_and_watch(direction, img):
             top_right = corner[0][1]
             bottom_right = corner[0][2]
             bottom_left = corner[0][3]
-
+            
+            # Compute the height in pixels
+            #height = bottom_left[1] - top_left[1]
+            #print("Height of the ArUco marker:", height, "pixels")
             print('Landmark detected')
             print(top_left, top_right, bottom_right, bottom_left)
 
-            while top_left[0] > 550 and top_left[0] < 350 and bottom_left[0] < y and bottom_left > z: # hvis landmark er ligeud
+            while top_left > x and top_left < y and bottom_left < y and bottom_left > z: # hvis landmark er ligeud
                 arlo.go_diff(leftWheelFactor*standardSpeed, rightWheelFactor*standardSpeed, 1, 1) 
-
-
-def drive_to_landmark():
-    top_left, top_right, bottom_right, bottom_left = camera('turn and watch') # drejer, indtil den finder et landmark
-    _utils.drive_and_sense()
-
-    arlo.stop()
-    turn_until_landmark_is_center
 
 
