5a349f42428e05d0c21b920cbaa3577eb7f7883e
Woergaard <thomas.w.kjaer@gmail.com>
2023-09-20 13:01:07 +0200

    Ex3 - 25. commit

--- Ex3/Ex3_old.py
+++ Ex3/Ex3_old.py
@@ -4,10 +4,6 @@ import cv2 # Import the OpenCV library
 import time
 from pprint import *
 import numpy as np
-import _utils
-import robot
-
-arlo = robot.Robot()
 
 try:
     import picamera2
@@ -18,11 +14,85 @@ except ImportError:
 
 print("OpenCV version = " + cv2.__version__)
 
+
+def height_of_QR_in_image(img):
+    '''
+    Funktionen returnerer højden af en box på et billede i pixels.n
+    Argumenter:
+        image:  det givne billede
+    '''
+    arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+    
+    aruco_corners, _, _ = cv2.aruco.detectMarkers(img, arucoDict) 
+    print(aruco_corners)
+
+
+    # If at least one marker is detected
+    if len(aruco_corners) > 0:
+        for corner in aruco_corners:
+            # The corners are ordered as top-left, top-right, bottom-right, bottom-left
+            top_left = corner[0][0]
+            bottom_left = corner[0][3]
+            
+            # Compute the height in pixels
+            height = bottom_left[1] - top_left[1]
+            print("Height of the ArUco marker:", height, "pixels")
+            print(top_left)
+
+"""
+def camera_setup():
+    # Open a camera device for capturing
+    imageSize = (1280, 720)
+    FPS = 30
+    cam = picamera2.Picamera2()
+    frame_duration_limit = int(1/FPS * 1000000) # Microseconds
+    # Change configuration to set resolution, framerate
+    picam2_config = cam.create_video_configuration({"size": imageSize, "format": 'RGB888'},
+                                                                controls={"FrameDurationLimits": (frame_duration_limit, frame_duration_limit)},
+                                                                queue=False)
+    cam.configure(picam2_config) # Not really necessary
+    cam.start(show_preview=False)
+
+    print(cam.camera_configuration()) # Print the camera configuration in use
+
+    time.sleep(1)  # wait for camera to setup
+
+
+    # Open a window
+    WIN_RF = "Example 1"
+    cv2.namedWindow(WIN_RF)
+    cv2.moveWindow(WIN_RF, 100, 100)
+
+
+    while cv2.waitKey(4) == -1: # Wait for a key pressed event
+        image = cam.capture_array("main")
+        
+        # Show frames
+        cv2.imshow(WIN_RF, image)
+
+        height_of_QR_in_image(image)
+    
+camera_setup()
+"""
+
+
+
 ### EX 3.1 ###
 
 # See focal.py file in Ex3 folder
 
 ### EX 3.2 ###
+
+### DEFINEREDE PARAMETRE ###
+import _utils
+import robot
+
+arlo = robot.Robot()
+
+rightWheelFactor = 1.0
+leftWheelFactor = 1.06225
+standardSpeed = 50.0
+
 
 def turn_and_watch(direction, img):
     '''
@@ -38,7 +108,7 @@ def turn_and_watch(direction, img):
         print("Indtast enten 'left' eller 'right' som retning")
         return
 
-    arlo.go_diff(_utils.leftWheelFactor*_utils.standardSpeed*0.6, _utils.rightWheelFactor*_utils.standardSpeed*0.6, r, l)
+    arlo.go_diff(leftWheelFactor*standardSpeed, rightWheelFactor*standardSpeed, r, l)
     
     arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
     aruco_corners, _, _ = cv2.aruco.detectMarkers(img, arucoDict)
@@ -56,8 +126,37 @@ def turn_and_watch(direction, img):
 
             return True
         
+    w, h = 1280, 720
+    focal_length = 1744.36 
+    camera_matrix = np.array([[focal_length, 0, w/2], [0, focal_length, h/2], [0, 0, 1]])
 
 
+#            if top_left[0] < 550 and top_left[0] > 350 and bottom_left[0] > 350 and bottom_left[0] < 550: # hvis landmark er ligeud
+#                arlo.go_diff(leftWheelFactor*standardSpeed, rightWheelFactor*standardSpeed, 1, 1)
+#            if top_left[0] > 550 and bottom_left[0] > 550:
+#                arlo.go_diff(leftWheelFactor*standardSpeed, rightWheelFactor*standardSpeed, 0, 1)
+
+
+#def drive_to_landmark():
+#    top_left, top_right, bottom_right, bottom_left = camera('turn and watch') # drejer, indtil den finder et landmark
+#    _utils.drive_and_sense()
+#
+#    arlo.stop()
+#    #turn_until_landmark_is_center
+
+
+
+################################################
+#       Caroline's bud på Pose_estimation      #
+################################################
+'''
+Pose estimation is the task of determining the
+camera's position and orientation in relation to
+one or more landmarks. There are many
+methods of positioning
+- frame, img
+'''
+             
 ''' 
 pose_estimation
     - ids, boxese id (1, 2, 3...,N)
@@ -115,7 +214,7 @@ def landmark_drive(direction, img, arucoDict):
 def camera():
     # Open a camera device for capturing
     imageSize = (1280, 720)
-    FPS = 60
+    FPS = 30
     cam = picamera2.Picamera2()
     frame_duration_limit = int(1/FPS * 1000000) # Microseconds
     # Change configuration to set resolution, framerate
@@ -136,15 +235,14 @@ def camera():
     cv2.moveWindow(WIN_RF, 100, 100)
     
     arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
-    #aruco_corners,ids, rejectedImgPoints = cv2.aruco.detectMarkers(image, arucoDict)
+    aruco_corners,ids, rejectedImgPoints = cv2.aruco.detectMarkers(image, arucoDict)
 
     while cv2.waitKey(4) == -1: # Wait for a key pressed event
         image = cam.capture_array("main")
         
         # Show frames
         cv2.imshow(WIN_RF, image)
-        #landmark_drive('left', image, arucoDict)
-        turn_and_watch('left', image)
+        landmark_drive('left', image, arucoDict)
 
 
         #if command == 'turn and watch':
