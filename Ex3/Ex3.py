a8045bcc326392321d155b9967d5fcfb0f98aa4d
Woergaard <thomas.w.kjaer@gmail.com>
2023-09-20 13:08:45 +0200

    Ex3 - 27. commit

--- Ex3/Ex3.py
+++ Ex3/Ex3.py
@@ -117,7 +117,7 @@ def landmark_drive(direction, img, arucoDict):
 def camera():
     # Open a camera device for capturing
     imageSize = (1280, 720)
-    FPS = 30
+    FPS = 120
     cam = picamera2.Picamera2()
     frame_duration_limit = int(1/FPS * 1000000) # Microseconds
     # Change configuration to set resolution, framerate
