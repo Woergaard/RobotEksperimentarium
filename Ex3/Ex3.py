637695ce0cf51d1537c8d8637e84358848044637
Woergaard <thomas.w.kjaer@gmail.com>
2023-09-20 13:45:38 +0200

    Ex3 - 38. commit

--- Ex3/Ex3.py
+++ Ex3/Ex3.py
@@ -131,15 +131,11 @@ def camera():
 
     time.sleep(1)  # wait for camera to setup
 
-
     # Open a window
     WIN_RF = "Example 1"
     cv2.namedWindow(WIN_RF)
     cv2.moveWindow(WIN_RF, 100, 100)
     
-    arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
-    #aruco_corners,ids, rejectedImgPoints = cv2.aruco.detectMarkers(image, arucoDict)
-
     while cv2.waitKey(4) == -1: # Wait for a key pressed event
         image = cam.capture_array("main")
         
@@ -149,11 +145,6 @@ def camera():
         if turn_and_watch('left', image) == True: 
             arlo.stop()
             time.sleep(2)      
-            
-
-
-        #if command == 'turn and watch':
-        #    turn_and_watch('left', image)
 
 camera()
 
