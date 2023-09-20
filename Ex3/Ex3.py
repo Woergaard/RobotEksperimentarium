0269b9d16164ffa8f4b7193cae79bbc122a8e37f
Woergaard <thomas.w.kjaer@gmail.com>
2023-09-20 15:37:07 +0200

    Ex3 - 60. commit

--- Ex3/Ex3.py
+++ Ex3/Ex3.py
@@ -64,81 +64,44 @@ def turn_and_watch(direction, img):
         _utils.wait(2.0)
 
         return False
+
+def drive_to_landmarks(landmarks_lst):
     
-'''
-Pose estimation is the task of determining the
-camera's position and orientation in relation to
-one or more landmarks. There are many
-methods of positioning
-- frame, img
-'''
-             
-''' 
-pose_estimation
-    - ids, boxese id (1, 2, 3...,N)
-    - corners, the corners of the box 
-    - rejectedImgPoints, the marker candidates that have been rejected during the identification step
-'''         
+    for landmark in landmarks_lst:
+        arlo.go_diff(_utils.leftWheelFactor*_utils.standardSpeed, _utils.rightWheelFactor*_utils.standardSpeed, 1, 1)
 
-#def pose_estimation(img, arucoDict): 
-   #Denne funktion skal finde positionen af et landmark i forhold til robotten og udregne vinklen og afstanden til landmarket."""
+    
 
-    # 1. Lave boks rundt om QR code 
-    # 2. Udregne vinklen og afstanden til QR code
+def pose_estimation(img, arucoDict): 
+    """Funktionen returnerer en liste af placeringer i kameraets koordinatsystem samt id på de givne QR-koder"""
+    """Denne funktion skal finde positionen af et landmark i forhold til robotten og udregne vinklen og afstanden til landmarket."""
+
+    # 1. Udregne afstanden til QR code - DONE 
+    # 2. Udregn vinkel til QR code 
     # 3. Dreje robotten, så den er vinkelret på QR code
     # 4. Køre fremad, indtil robotten er tæt nok på QR code
-    
-#    aruco_corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(img, arucoDict)
-#    w, h = 1280, 720
-#    focal_length = 1744.36 
-#    camera_matrix = np.array([[focal_length, 0, w/2], [0, focal_length, h/2], [0, 0, 1]])
-#    arucoMarkerLength = 145.0
-#    distortion = 0
-    # Draw the detectet markers, if there is at least 1 marker
-    
-#    rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(aruco_corners, arucoMarkerLength, camera_matrix, distortion )
-#    print(tvec)
- 
-    #cv2.aruco.solvePnP(img, camera_matrix, distortion)
-    
-    #cv.aruco.estimatePoseSingleMarkers(	corners, markerLength, cameraMatrix, distCoeffs[, rvecs[, tvecs[, objPoints[, estimateParameters]]]
-    #length_of_axis = 0.01
-    
-    #imaxis = cv2.aruco.drawDetectedMarkers(img, aruco_corners, ids)
-    #for i in range(len(tvecs)):
-    #    imaxis = cv2.aruco.drawAxis(imaxis, camera_matrix, distortion, tvecs[i], length_of_axis)
 
-
-
-    
-        
-#        rvecs, tvecs, objPoints = cv2.aruco.estimatePoseSingleMarkers(aruco_corners, arucoMarkerLength, camera_matrix)#, distortion_coeffs) 
-        # Draw a square around the markers
-#        cv2.aruco.drawDetectedMarkers(img, aruco_corners, ids) 
-
-        # Draw Axis
-#        cv2.aruco.drawAxis(img, camera_matrix, rvecs, tvecs, 0.01)  
-
-#   return tvecs
-
-
-def pose_estimation_sofie(img, arucoDict): 
-    """Funktionen returnerer en liste af placeringer i kameraets koordinatsystem samt id på de givne QR-koder"""
-    
     aruco_corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(img, arucoDict)
     w, h = 1280, 720
     focal_length = 1744.36 
     camera_matrix = np.array([[focal_length, 0, w/2], [0, focal_length, h/2], [0, 0, 1]])
     arucoMarkerLength = 145.0
     distortion = 0
-     
-    _, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(aruco_corners, arucoMarkerLength, camera_matrix, distortion)
     
+    # Giver distancen til boxen
+    rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(aruco_corners, arucoMarkerLength, camera_matrix, distortion)
+    
+    rmat, jacobian = cv2.Rodrigues(rvecs)
+    world_angle = np.arccos(rmat[0][0])
+
     lst = []
     for i in range(len(ids)):
-        lst.append((linalg.norm(tvecs[i]), ids[i]))
-
+        lst.append((linalg.norm(tvecs[i]), world_angle, ids[i][0]))
+    
+    lst = lst.sort()
     print(lst)
+    return lst
+
 
 
 def camera():
@@ -172,7 +135,8 @@ def camera():
         #landmark_drive('left', image, arucoDict)
         if turn_and_watch('left', image) == True: 
             arlo.stop()
-            pose_estimation_sofie(image, arucoDict)
+            landmarks_lst = pose_estimation(image, arucoDict)
+            drive_to_landmarks(landmarks_lst)
             time.sleep(15)      
 
 camera()
