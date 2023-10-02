34df1387c7072ef7563818895057086125c09624
Woergaard <thomas.w.kjaer@gmail.com>
2023-10-02 15:27:18 +0200

    Ex4 - 41. commit

--- Ex4/Ex4.py
+++ Ex4/Ex4.py
@@ -168,12 +168,12 @@ def steer(nearest_node, steering_node, stepLength):
     #x = nearest_node.x - (edge.x*stepLength)/nævner
     #z = nearest_node.z - (edge.z*stepLength)/nævner
 
-    nævner = np.sqrt(steering_node.x**2 + steering_node.z**2)
-    x = (nearest_node.x - edge.x) * stepLength / nævner + steering_node.x
-    z = (nearest_node.z - edge.z) * stepLength / nævner + steering_node.z
+    #nævner = np.sqrt(steering_node.x**2 + steering_node.z**2)
+    #x = (nearest_node.x - edge.x) * stepLength / nævner + steering_node.x
+    #z = (nearest_node.z - edge.z) * stepLength / nævner + steering_node.z
     
-    #x = (nearest_node.x - edge.x) * stepLength / np.sqrt((nearest_node.x - edge.x)**2)
-    #z = (nearest_node.z - edge.z) * stepLength / np.sqrt((nearest_node.z - edge.z)**2)
+    x = (nearest_node.x - steering_node.x) * stepLength / np.sqrt((nearest_node.x - steering_node.x)**2)
+    z = (nearest_node.z - steering_node.z) * stepLength / np.sqrt((nearest_node.z - steering_node.z)**2)
     
     new_node = Node(x, z, nearest_node)
     return new_node
