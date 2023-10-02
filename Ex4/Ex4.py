eb47b625c5135ed7da09ed16536880ac12af2698
Woergaard <thomas.w.kjaer@gmail.com>
2023-10-02 15:55:57 +0200

    Ex4 - 52. commit

--- Ex4/Ex4.py
+++ Ex4/Ex4.py
@@ -171,17 +171,25 @@ def steer(nearest_node, steering_node, stepLength):
     #z = nearest_node.z - (edge.z*stepLength)/nævner
 
     #nævner = np.sqrt(steering_node.x**2 + steering_node.z**2)
-    x = (nearest_node.x - steering_node.x)  / linalg.norm(nearest_node.x - steering_node.x)#nævner + steering_node.x
-    z = (nearest_node.z - steering_node.z) / linalg.norm(nearest_node.z - steering_node.z) #nævner + steering_node.z
+    #x = (nearest_node.x - steering_node.x)  / linalg.norm(nearest_node.x - steering_node.x)#nævner + steering_node.x
+    #z = (nearest_node.z - steering_node.z) / linalg.norm(nearest_node.z - steering_node.z) #nævner + steering_node.z
     
-    print("x:", x, "z:", z)
+    #print("x:", x, "z:", z)
 
-    x = steering_node.x + x * stepLength
-    z = steering_node.z + z * stepLength
+    #x = steering_node.x + x * stepLength
+    #z = steering_node.z + z * stepLength
 
-    print("x:", x, "z:", z)
+    #print("x:", x, "z:", z)
+
+    ### Asgers Numpy Vectors ###
+    near_node = np.array([nearest_node.x, nearest_node.z])
+    steer_node = np.array([steering_node.x, steering_node.z])
+    xz = near_node - steer_node
+    magnitude = np.sqrt(xz[0]**2 + xz[1]**2) # vectors længde
+    new_vec = (xz * stepLength)/magnitude
+    ### END ###
     
-    new_node = Node(x, z, nearest_node)
+    new_node = Node(new_vec[0], new_vec[1], nearest_node)
     return new_node
 
 ### RRT ###
