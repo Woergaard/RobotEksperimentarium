e71e5604801e6ee1eaa152eda47cde66e69f16da
Woergaard <thomas.w.kjaer@gmail.com>
2023-10-02 13:27:35 +0200

    Ex4 - 14. commit

--- Ex4/Ex4.py
+++ Ex4/Ex4.py
@@ -23,7 +23,8 @@ import matplotlib.pyplot as plt
 #radius_arlo = 22.5
 #radius_QR = 17.5 
 
-### EXERCISE 1 ###
+
+### KLASSER ###
 
 class Landmark:
     def __init__(self, distance, vinkel, retning, id, tvec):
@@ -34,111 +35,6 @@ class Landmark:
         self.tvec = tvec
         self.x = tvec[0]
         self.z = tvec[2]
-
-def landmark_detection(img, arucoDict): 
-    """Funktionen returnerer en liste af placeringer i kameraets koordinatsystem samt id på de givne QR-koder"""
-    """Denne funktion skal finde positionen af et landmark i forhold til robotten og udregne vinklen og afstanden til landmarket."""
-
-    aruco_corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(img, arucoDict)
-    w, h = 1280, 720
-    focal_length = 1744.36 
-    camera_matrix = np.array([[focal_length, 0, w/2], [0, focal_length, h/2], [0, 0, 1]])
-    arucoMarkerLength = 145.0
-    distortion = 0
-    
-    # Giver distancen til boxen
-    _, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(aruco_corners, arucoMarkerLength, camera_matrix, distortion)
-    
-    lst = []
-    
-    for i in range(len(ids)):
-        world_angle = np.arccos(np.dot(tvecs[i]/np.linalg.norm(tvecs[i]), np.array([0, 0, 1])))
-        if np.dot(tvecs[i], np.array([1, 0, 0])) < 0:
-            direction = 'left'
-        else:
-            direction = 'right'
-
-        lst.append(Landmark(linalg.norm(tvecs[i]), world_angle, direction, ids[i][0], tvecs[i][0]))
-        print('dist: ' + str(linalg.norm(tvecs[i])) + ', vinkel: ' + str(world_angle) + direction + ', id: ' + str(ids[i][0]) + ', tvec: ' + str(tvecs[i][0]))
-    
-    lst.sort(key=lambda x: x.distance, reverse=False)
-
-    return lst
-
-# def build_map(landmarks):
-#     plt.plot(0,0, 'bo')
-#     plt.annotate('ArloCinque', xy=(0, 0))
-
-#     for landmark in landmarks:
-#         x = landmark.tvec[0]
-#         z = landmark.tvec[2]
-#         plt.plot(x,z, 'ro')
-#         #plt.Circle((x, z), 175)
-#         plt.annotate(str(landmark.id), xy=(x,z))
-    
-#     plt.ylim(0, 10000)
-#     plt.xlim(-10000, 10000)
-#     plt.xlabel('x')
-#     plt.ylabel('z')
-#     plt.show()
-
-# def is_spot_free(spotx, spotz, landmarks):
-#     box_radius = 175.0
-
-#     for landmark in landmarks:        
-#         if np.sqrt((spotx-landmark.x)+(spotz-landmark.z)) < box_radius:
-#             print('Occupied by ' + str(landmark.id))
-#             return False
-
-#     print('Spot free!')
-#     return True
-
-
-##### KØRSEL ####
-
-# def camera(command):
-#     # Open a camera device for capturing
-#     imageSize = (1280, 720)
-#     FPS = 60
-#     cam = picamera2.Picamera2()
-#     frame_duration_limit = int(1/FPS * 1000000) # Microseconds
-#     # Change configuration to set resolution, framerate
-#     picam2_config = cam.create_video_configuration({"size": imageSize, "format": 'RGB888'},
-#                                                                 controls={"FrameDurationLimits": (frame_duration_limit, frame_duration_limit)},
-#                                                                 queue=False)
-#     cam.configure(picam2_config) # Not really necessary
-#     cam.start(show_preview=False)
-
-#     print(cam.camera_configuration()) # Print the camera configuration in use
-
-#     time.sleep(1)  # wait for camera to setup
-
-#     arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
-#     # Open a window
-#     WIN_RF = "Example 1"
-#     cv2.namedWindow(WIN_RF)
-#     cv2.moveWindow(WIN_RF, 100, 100)
-    
-#     while cv2.waitKey(4) == -1: # Wait for a key pressed event
-#         image = cam.capture_array("main")
-        
-#         # Show frames
-#         cv2.imshow(WIN_RF, image)
-#         #landmark_drive('left', image, arucoDict)
-
-#         if command == 'build_map':
-#             landmarks = landmark_detection(image, arucoDict)
-#             build_map(landmarks)
-#             is_spot_free(574, 846, landmarks)
-#             sleep(2)
-
-# camera('build_map')
-
-
-### EXERCISE 2 ### (TING ER DOBBELT)
-
-### KLASSER ###
-
 class Node:
     def __init__(self, x, z, parent):
         self.x = x
@@ -190,6 +86,36 @@ class Map:
 def dist(node1, node2):
     return np.sqrt((node1.x-node2.x)**2+(node1.z-node2.z)**2) 
 
+def landmark_detection(img, arucoDict): 
+    """Funktionen returnerer en liste af placeringer i kameraets koordinatsystem samt id på de givne QR-koder"""
+    """Denne funktion skal finde positionen af et landmark i forhold til robotten og udregne vinklen og afstanden til landmarket."""
+
+    aruco_corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(img, arucoDict)
+    w, h = 1280, 720
+    focal_length = 1744.36 
+    camera_matrix = np.array([[focal_length, 0, w/2], [0, focal_length, h/2], [0, 0, 1]])
+    arucoMarkerLength = 145.0
+    distortion = 0
+    
+    # Giver distancen til boxen
+    _, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(aruco_corners, arucoMarkerLength, camera_matrix, distortion)
+    
+    lst = []
+    
+    for i in range(len(ids)):
+        world_angle = np.arccos(np.dot(tvecs[i]/np.linalg.norm(tvecs[i]), np.array([0, 0, 1])))
+        if np.dot(tvecs[i], np.array([1, 0, 0])) < 0:
+            direction = 'left'
+        else:
+            direction = 'right'
+
+        lst.append(Landmark(linalg.norm(tvecs[i]), world_angle, direction, ids[i][0], tvecs[i][0]))
+        print('dist: ' + str(linalg.norm(tvecs[i])) + ', vinkel: ' + str(world_angle) + direction + ', id: ' + str(ids[i][0]) + ', tvec: ' + str(tvecs[i][0]))
+    
+    lst.sort(key=lambda x: x.distance, reverse=False)
+
+    return lst
+
 def is_spot_free(spot, landmarks, self, animation=True, writer=None):
     box_radius = 175.0
 
@@ -206,6 +132,7 @@ def is_spot_free(spot, landmarks, self, animation=True, writer=None):
             if writer is not None:
                 writer.grab_frame()
 
+#####
     print('Spot free!')
     return True
 
@@ -254,6 +181,45 @@ def RRT(goal, mapsizex, mapsizez, maxiter, landmarks, rootNode, stepLength):
 from matplotlib.animation import FFMpegWriter
 
 
+
+
+def run_RRT(img, arucoDict): 
+    landmarks = landmark_detection(img, arucoDict)
+    goal = landmarks[0]
+
+    rootNode = Node(0, 0, None)
+    stepLength = 100 # milimeter
+    maxiter = 500
+
+    ourMap = Map(10000, 10000)
+
+    G, new_node = RRT(goal, ourMap.xlim, ourMap.zlim, maxiter, landmarks, rootNode, stepLength)
+    
+    ourMap.draw_landmarks(landmarks)
+    ourMap.draw_tree(G)
+    ourMap.show_map()
+    
+
+    
+    ### animation begynder her
+
+    show_animation = True 
+    
+    FFMpegWriter = animation.writers['ffmpeg']
+    metadata = dict(title='Movie Test', artist='Matplotlib',
+                comment='RRT')
+    writer = FFMpegWriter(fps=15, metadata=metadata)
+    
+    fig = plt.figure()
+
+    with writer.saving(fig, "writer_test.mp4", 100):
+        path = RRT.__animation(animation=show, writer=writer)
+        
+        writer.grab_frame()
+        
+
+########
+
 def camera(command):
     # Open a camera device for capturing
     imageSize = (1280, 720)
@@ -284,57 +250,26 @@ def camera(command):
         cv2.imshow(WIN_RF, image)
         #landmark_drive('left', image, arucoDict)
 
+        if command == 'build_map':
+            landmarks = landmark_detection(image, arucoDict)
+
+            ourMap = Map(10000, 10000)
+            ourMap.draw_landmarks(landmarks)
+
+            is_spot_free(574, 846, landmarks)
+            sleep(2)
+
         if command == 'RRT':
             run_RRT(image, arucoDict)
 
 camera('RRT')
 
 
-
-def run_RRT(img, arucoDict): 
-    landmarks = landmark_detection(img, arucoDict)
-    goal = landmarks[0]
-
-    rootNode = Node(0, 0, None)
-    stepLength = 100 # milimeter
-    maxiter = 500
-
-    ourMap = Map(10000, 10000)
-
-    G, new_node = RRT(goal, ourMap.xlim, ourMap.zlim, maxiter, landmarks, rootNode, stepLength)
-    
-    ourMap.draw_landmarks(landmarks)
-    ourMap.draw_tree(G)
-    ourMap.show_map()
-    
-
-    
-
-
-
-
-
-
-    show_animation = True 
-    
-    FFMpegWriter = animation.writers['ffmpeg']
-    metadata = dict(title='Movie Test', artist='Matplotlib',
-                comment='RRT')
-    writer = FFMpegWriter(fps=15, metadata=metadata)
-    
-    fig = plt.figure()
-
-    with writer.saving(fig, "writer_test.mp4", 100):
-        path = RRT.__animation(animation=show, writer=writer)
-        
-        writer.grab_frame()
-
-
+'''
 if __name__ == '__main__':
     main()
 
 
-'''
             # NÅET TIL
             steer
             stepLength = 
