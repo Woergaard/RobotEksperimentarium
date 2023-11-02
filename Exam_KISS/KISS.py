0d09f7bc7248e1bbc9ed226186c6f4dd18a87570
Woergaard <thomas.w.kjaer@gmail.com>
2023-11-02 12:30:54 +0100

    Ex6 - 216. commit

--- Exam_KISS/KISS.py
+++ Exam_KISS/KISS.py
@@ -71,15 +71,15 @@ def costaldrive(goalID, image, arucoDict, frontLimitCoastal, sideLimitCoastal):
 #                    arlo.go_diff(leftWheelFactor*standardSpeed, rightWheelFactor*standardSpeed, 1, 1)
             #if landmarkFound:
         # tag højde for at der er tomt
-            while pingFront > frontLimitCoastal and pingLeft > sideLimitCoastal-650 and pingRight > sideLimitCoastal:
-                pingFront, pingLeft, pingRight, pingBack = _utils.sensor()
-                arlo.go_diff(leftWheelFactor*standardSpeed, rightWheelFactor*standardSpeed, 1, 1)
-            
-            _utils.sharp_turn('left', 90)
+        while pingFront > frontLimitCoastal and pingLeft > sideLimitCoastal-650 and pingRight > sideLimitCoastal:
+            pingFront, pingLeft, pingRight, pingBack = _utils.sensor()
+            arlo.go_diff(leftWheelFactor*standardSpeed, rightWheelFactor*standardSpeed, 1, 1)
+        
+        _utils.sharp_turn('left', 90)
 
-            while pingFront > frontLimitCoastal and pingLeft > sideLimitCoastal-650 and pingRight > sideLimitCoastal:
-                pingFront, pingLeft, pingRight, pingBack = _utils.sensor()
-                arlo.go_diff(leftWheelFactor*standardSpeed, rightWheelFactor*standardSpeed, 1, 1)
+        while pingFront > frontLimitCoastal and pingLeft > sideLimitCoastal-650 and pingRight > sideLimitCoastal:
+            pingFront, pingLeft, pingRight, pingBack = _utils.sensor()
+            arlo.go_diff(leftWheelFactor*standardSpeed, rightWheelFactor*standardSpeed, 1, 1)
             
 
     if goalID == 1:
