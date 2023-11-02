8724a344ab616b7c7d385321721095ce0afe1d3c
Woergaard <thomas.w.kjaer@gmail.com>
2023-11-02 12:33:09 +0100

    Ex6 - 217. commit

--- Exam_KISS_SELFLOCALIZE/_utils.py
+++ Exam_KISS_SELFLOCALIZE/_utils.py
@@ -742,7 +742,7 @@ def distance_for_particle(particle_i, landmark):
     d_i = dist(particle_i, landmark)
     return d_i
 
-import decimal
+#import decimal
 
 def distance_weights(d_M, d_i, sigma_d):
     '''
@@ -754,15 +754,15 @@ def distance_weights(d_M, d_i, sigma_d):
 
     print('Første led: ', (1 / nævner1))
     #print('Andet led: ', math.exp(-(tæller2 / nævner2)))
-    print('Andet led: ', decimal.Decimal(-(tæller2 / nævner2)).exp())
+    print('Andet led: ', np.exp(-(tæller2 / nævner2)))
     print('tæller2: ', tæller2)
     print('nævner2: ', nævner2)
     print('Indmad: ', -(tæller2 / nævner2))
 
 
     #return (1 / nævner1) * math.exp(-(tæller2 / nævner2))
-    print('returnerer:', (1 / nævner1) * decimal.Decimal(-(tæller2 / nævner2)).exp())
-    return (1 / nævner1) * decimal.Decimal(-(tæller2 / nævner2)).exp()
+    print('returnerer:', (1 / nævner1) * np.exp(-(tæller2 / nævner2)))
+    return (1 / nævner1) * np.exp(-(tæller2 / nævner2))
 
 def orientation_distribution(phi_M, sigma_theta, particle, landmark):
     theta_i, lx, ly, x_i, y_i = particle.theta, landmark.x, landmark.z, particle.x, particle.y
