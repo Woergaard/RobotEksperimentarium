45a2664bf3b04f7fe4df427725a60473430d5f1f
Woergaard <thomas.w.kjaer@gmail.com>
2023-11-06 11:25:01 +0100

    Ex6 - 289. commit

--- Exam_FINAL/Exam.py
+++ Exam_FINAL/Exam.py
@@ -586,8 +586,6 @@ def robo_rally(landmarkIDs, landmarks_dict, landmark_colors, showcamera, show, f
             lost = True
             iterations = 0
 
-            
-
             while lost:
                 if iterations < 18:
                     found = use_camera(cam, arucoDict, 'turn_and_watch', [landmarkIDs], showcamera, show)
@@ -604,11 +602,11 @@ def robo_rally(landmarkIDs, landmarks_dict, landmark_colors, showcamera, show, f
             
             arlo.stop()
             print('Begynder selflokalisering.')
-            params = [3, landmarkIDs, landmarks_dict, landmark_colors, arlo_position, particles, [], stepLength]
-            arlo_position, particles = selflocalize_360_degrees(cam, arucoDict, show, showcamera, params)
+            #params = [3, landmarkIDs, landmarks_dict, landmark_colors, arlo_position, particles, [], stepLength]
+            #arlo_position, particles = selflocalize_360_degrees(cam, arucoDict, show, showcamera, params)
             #arlo_position, particles = use_camera(cam, arucoDict, '360_selflocalize', [3, landmarkIDs, landmarks_dict, landmark_colors, arlo_position, particles, [], stepLength], showcamera, show)
             
-            #arlo_position, particles = use_camera(cam, arucoDict, 'selflocalize', [3, landmarkIDs, landmarks_dict, landmark_colors, arlo_position, particles, path, stepLength], showcamera, show)
+            arlo_position, particles = use_camera(cam, arucoDict, 'selflocalize', [3, landmarkIDs, landmarks_dict, landmark_colors, arlo_position, particles, [], stepLength], showcamera, show)
             
             #print(math.floor(arlo_position.x), math.floor(arlo_position.z), math.floor(arlo_position.theta))
             #arlo_node = _utils.Node(arlo_position.x, arlo_position.z, None)
@@ -628,11 +626,11 @@ def robo_rally(landmarkIDs, landmarks_dict, landmark_colors, showcamera, show, f
                     print('RRT-træ betod kun af Arlos position, æv :(')
 
 
-            params = [3, landmarkIDs, landmarks_dict, landmark_colors, arlo_position, particles, path, stepLength]
-            arlo_position, particles = selflocalize_360_degrees(cam, arucoDict, show, showcamera, params)
+            #params = [3, landmarkIDs, landmarks_dict, landmark_colors, arlo_position, particles, path, stepLength]
+            #arlo_position, particles = selflocalize_360_degrees(cam, arucoDict, show, showcamera, params)
 
             #arlo_position, particles = use_camera(cam, arucoDict, '360_selflocalize', [3, landmarkIDs, landmarks_dict, landmark_colors, arlo_position, particles, path, stepLength], showcamera, show)
-            #arlo_position, particles = use_camera(cam, arucoDict, 'selflocalize', [10, landmarkIDs, landmarks_dict, landmark_colors, arlo_position, particles, path, stepLength], showcamera, show)
+            arlo_position, particles = use_camera(cam, arucoDict, 'selflocalize', [3, landmarkIDs, landmarks_dict, landmark_colors, arlo_position, particles, path, stepLength], showcamera, show)
             landmarkfound = landmark_reached(arlo_position, temp_goal_Node)
                     
         if landmarkfound:
