b70052af7972d1ea419c2f4fadb600b231ccda04
Woergaard <thomas.w.kjaer@gmail.com>
2023-11-06 11:11:47 +0100

    Ex6 - 286. commit

--- Exam_FINAL/Exam.py
+++ Exam_FINAL/Exam.py
@@ -95,8 +95,7 @@ def selflocalize(cam, showGUI, maxiters, landmarkIDs, landmarks_dict, landmark_c
             #print(objectIDs)
             #print(type(dists))
 
-            print('nået hertil')
-            if isinstance(objectIDs, list):
+            if objectIDs == 'intet':
             #if not 'numpy' in str(type(objectIDs)):
                 # No observation - reset weights to uniform distribution
                 print('selflocalization detekterede ikke nogle landmarks.')
--- Exam_FINAL/camera.py
+++ Exam_FINAL/camera.py
@@ -328,9 +328,9 @@ class Camera(object):
                 direction = -1*np.sign(tobj_xz[0,0])  # The same as np.sign(np.dot(tobj, xaxis.T))
                 angles[i] = direction * np.arccos(np.dot(tobj_xz, zaxis.T))
         else:
-            dists = []
-            ids = []
-            angles = []
+            dists = 'intet'
+            ids = 'intet'
+            angles = 'intet'
             '''
             dists = None
             ids = None
